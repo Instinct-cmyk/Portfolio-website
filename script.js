ad7627fa64748446155fd0d836878608d59ba1f8
Instinct-cmyk <yuvrajdabral@gmail.com>
2024-12-12 23:21:47 +0530

    added index.html

---
+++ script.js
@@ -0,0 +1,9 @@
+function openModal(modalId) {
+    document.getElementById(`modal-${modalId}`).style.display = "block";
+  }
+  
+  function closeModal(modalId) {
+    document.getElementById(`modal-${modalId}`).style.display = "none";
+  }
+  
+  
